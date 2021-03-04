2fb11c2527c5412ac81d89f224afe4ee70ef20c9
Florent-Vanhollebeke <florent.vanhollebeke@gmail.com>
2021-03-04 11:59:26 +0100

    Modifications aux views et templates, sessentiellement sur list_person et listÃ_file

--- mysite/uploads/models.py
+++ mysite/uploads/models.py
@@ -9,13 +9,13 @@ from django.db import models
 
 
 class Person(models.Model):
-    
+
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return  f"{self.first_name}, {self.last_name}"
+        return f"{self.first_name}, {self.last_name}"
 
 
 """
